06f422f01a7eaaa98777ad8999ec367a713932a4
william quimbay <quimbayfeo@gmail.com>
2024-12-05 19:57:11 -0500

    python

---
+++ presentacion.py
@@ -0,0 +1,4 @@
+# ejercico de presentacio
+print("nombre: william quimbay")
+print("edad:" , 33)
+print("pais: colombia")
---
+++ variables.py
@@ -0,0 +1,6 @@
+# Decalacion de variables  y asignacion de valores
+
+nombre = "william"
+edad = 33
+peso = 75.2
+casado = False
